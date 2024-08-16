b7af3d02bf8b0fb607542c23b4d23dd7b1fbdd8b
Mominul <islammominul3378@gmail.com>
2024-08-16 06:00:09 +0600

    added cart collection api email database send

--- index.js
+++ index.js
@@ -46,7 +46,13 @@ async function run() {
       res.send(result);
     })
 
-    // cart collection 
+    // cart collection
+    app.get('/carts', async(req, res) => {
+      const email = req.query.email;
+      const query = {email: email};
+      const result = await cartCollection.find(query).toArray();
+      res.send(result);
+    }) 
     app.post('/carts', async(req, res) => {
       const cartItem = req.body;
       const result = await cartCollection.insertOne(cartItem);
