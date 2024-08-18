79c1b0a34f2d75f97e1db1f302947c17830f56f5
Mominul <islammominul3378@gmail.com>
2024-08-18 06:03:17 +0600

    added all users admin dashboard server side

--- index.js
+++ index.js
@@ -40,6 +40,10 @@ async function run() {
 
 
     // user related api 
+    app.get('/users', async(req, res) => {
+      const result = await userCollection.find().toArray();
+      res.send(result);
+    } )
     app.post('/users', async(req, res)=> {
       const user = req.body; 
       const query = {email: user.email}
@@ -51,8 +55,26 @@ async function run() {
       res.send(result);
     })
 
+    app.delete('/users/:id', async(req, res) => {
+      const id = req.params.id;
+      const query = {_id: new ObjectId(id)};
+      const result = await userCollection.deleteOne(query)
+      res.send(result);
+    })
 
+    app.patch('/users/admin/:id', async(req, res) => {
+      const id = req.params.id;
+      const filter = {_id: new ObjectId(id)};
+      const updatedDoc = {
+        $set: {
+          role: 'admin'
+        }
+      }
+      const result = await userCollection.updateOne(filter, updatedDoc)
+      res.send(result);
+    })
 
+    // menu collection
     app.get('/menu', async(req, res) => {
       const result = await menuCollection.find().toArray();
       res.send(result);
