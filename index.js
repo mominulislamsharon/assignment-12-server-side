608b5cf79b0541fde8195c29568a552f7709584f
Mominul <islammominul3378@gmail.com>
2024-08-21 03:07:41 +0600

    agreate pipeline

--- index.js
+++ index.js
@@ -270,6 +270,42 @@ async function run() {
       })
     })
 
+    // using aggregate pipeline 
+    app.get('/order-stats', verifyToken,verifyAdmin, async(req, res) => {
+      const result = await paymentCollection.aggregate([
+        {
+          $unwind: '$menuItemIds'
+        }, 
+        {
+          $lookup : {
+            from: 'menu', 
+            localField: 'menuItemIds',
+            foreignField: '_id',
+            as: 'menuItems'
+          }
+        }, 
+        {
+          $unwind: '$menuItems'
+        }, 
+        {
+          $group: {
+            _id: '$menuItems.category', 
+            quantity: { $sum: 1 },
+            revenue: { $sum: '$menuItems.price' }
+          }
+        }, 
+        {
+          $project: {
+            _id: 0,
+            category: '$_id',
+            quantity: '$quantity',
+            revenue: '$revenue'
+          }
+        }
+      ]).toArray();
+      res.send(result);
+    })
+
 
 
 
